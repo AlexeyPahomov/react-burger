94e944c18cceecaaebaf45db68ea5d80b1822a20
Алексей Пахомов <a.pahomov@mkskom.ru>
2026-02-27 17:27:34 +0300

    refactor: ссылки меню хедера в отдельный компонент

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,10 +1,6 @@
-import {
-  BurgerIcon,
-  ListIcon,
-  Logo,
-  ProfileIcon,
-} from '@krgaa/react-developer-burger-ui-components';
-import { NavLink } from 'react-router-dom';
+import { Logo } from '@krgaa/react-developer-burger-ui-components';
+
+import HeaderLink from './components/header-link/header-link';
 
 import styles from './app-header.module.css';
 
@@ -13,37 +9,18 @@ export const AppHeader = (): React.JSX.Element => {
     <header className={styles.header}>
       <nav className={`${styles.menu} p-4`}>
         <div className={styles.menu_part_left}>
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              `${styles.link} ${isActive ? styles.link_active : ''}`
-            }
-          >
-            <BurgerIcon type="primary" />
-            <p className="text text_type_main-default ml-2">Конструктор</p>
-          </NavLink>
-          <NavLink
-            to="/feed"
-            className={({ isActive }) =>
-              `${styles.link} ml-10 ${isActive ? styles.link_active : ''}`
-            }
-          >
-            <ListIcon type="secondary" />
-            <p className="text text_type_main-default ml-2">Лента заказов</p>
-          </NavLink>
+          <HeaderLink title="Конструктор" to="/" icon="burger" />
+          <HeaderLink title="Лента заказов" to="/feed" icon="list" extraClass="ml-10" />
         </div>
         <div className={styles.logo}>
           <Logo />
         </div>
-        <NavLink
+        <HeaderLink
+          title="Личный кабинет"
           to="/profile"
-          className={({ isActive }) =>
-            `${styles.link} ${styles.link_position_last} ${isActive ? styles.link_active : ''}`
-          }
-        >
-          <ProfileIcon type="secondary" />
-          <p className="text text_type_main-default ml-2">Личный кабинет</p>
-        </NavLink>
+          icon="profile"
+          extraClass={styles.link_position_last}
+        />
       </nav>
     </header>
   );
